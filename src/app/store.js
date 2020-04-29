c168c98d5ac3b5a896fc820bde04a41acb001bd8
David Le Jolly <zahara.alnour@gmail.com>
2020-04-29 12:19:20 +0300

    Persist Store

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,32 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import authReducer from 'features/auth/authSlice'
 import mentalReducer from 'features/mental/mentalSlice'
 import dbReducer from 'features/db/dbSlice'
+import { persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 
+// const reducers = combineReducers({
+//   auth:authReducer,
+//   mental:mentalReducer,
+//   db:dbReducer
+// })
+
+// const persistConfig = {
+//   key: 'root',
+//   storage
+// }
+
+// const persistedReducer = persistReducer(persistConfig, reducers)
+
+
+// const store = configureStore({
+//   reducer: persistedReducer,
+// })
+
+// const persistore = persistStore(store)
+
+// export {store, persistore}
 
 export default configureStore({
   reducer: {
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './app/App'
+// import {store, persistore} from './app/store'
 import store from './app/store'
+import { PersistGate } from 'redux-persist/integration/react'
 import { Provider } from 'react-redux'
 import * as serviceWorker from './app/serviceWorker'
 import { BrowserRouter as Router } from 'react-router-dom'
@@ -13,9 +15,11 @@ const hist = createBrowserHistory()
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store} history={hist}>
+    {/* <PersistGate loading={null} persistor={persistore}> */}
       <Router>
         <App />
       </Router>
+      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
