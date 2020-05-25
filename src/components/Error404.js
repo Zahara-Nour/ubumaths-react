90754a41a116bc522b7923ba59c03dde7f8a8063
David Le Jolly <zahara.alnour@gmail.com>
2020-05-25 10:57:12 +0300

    Add jss style for error404 page

--- src/components/Error404.js
+++ src/components/Error404.js
@@ -3,18 +3,21 @@ import spiralPi from 'assets/img/spiral-pi-clean.png'
 import { Container } from '@material-ui/core'
 import GridContainer from './Grid/GridContainer'
 import GridItem from './Grid/GridItem'
-import './Error404.css'
+import styles from 'assets/jss/components/error404Style.js'
+import { makeStyles } from '@material-ui/core/styles'
+const useStyles = makeStyles(styles)
 
 function Error404() {
+  const classes = useStyles()
   return (
     <Container fixed>
-      <h3>
-        Cette page est introuvable, vraissemblablement perdue dans les décimales
-        de pi !
-      </h3>
+      <h2>
+        Page introuvable !
+       
+      </h2>
       <GridContainer justify='center'>
         <GridItem xs={10} md={6}>
-          <img className='spin' src={spiralPi} alt='pi decimals' width='100%' />
+          <img className={classes["pi-spiral"]} src={spiralPi} alt='pi decimals' width='100%' />
         </GridItem>
       </GridContainer>
     </Container>
