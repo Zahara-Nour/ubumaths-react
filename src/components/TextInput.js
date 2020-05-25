7f917d2850cd0938c00ec985968fe6f7c3f72044
David Le Jolly <zahara.alnour@gmail.com>
2020-05-25 10:57:59 +0300

    Add TextInput  multiline parameter

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -1,25 +1,30 @@
 import React from 'react'
 import CustomInput from 'components/CustomInput/CustomInput'
 
-function TextInput({label, text, onChange, success=false, error=false}) {
-
-    return (
-        <CustomInput
-            labelText={label}
-            success={success}
-            error={error}
-            formControlProps={{
-              fullWidth: true,
-            }}
-            inputProps={{
-              type: 'text',
-              value: text || '', // to ensure input being considered as a controlled component,
-              onChange: (evt) => onChange(evt.target.value),
-              multiline:true
-            }}
-          />
-    )
-
+function TextInput({
+  label,
+  text,
+  onChange,
+  success = false,
+  error = false,
+  multiline = false,
+}) {
+  return (
+    <CustomInput
+      labelText={label}
+      success={success}
+      error={error}
+      formControlProps={{
+        fullWidth: true,
+      }}
+      inputProps={{
+        type: 'text',
+        value: text || '', // to ensure input being considered as a controlled component,
+        onChange: (evt) => onChange(evt.target.value),
+        multiline,
+      }}
+    />
+  )
 }
 
 export default TextInput
