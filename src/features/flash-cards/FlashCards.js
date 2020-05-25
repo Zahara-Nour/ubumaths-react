29e5b7b5a826bf8f0de84545ed4ebf1897d7d092
David Le Jolly <zahara.alnour@gmail.com>
2020-05-25 15:59:45 +0300

    As react helmet

--- src/features/flash-cards/FlashCards.js
+++ src/features/flash-cards/FlashCards.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Helmet } from 'react-helmet'
 import { Switch, Route } from 'react-router-dom'
 import DisplayFlashCards from './DisplayFlashCards'
 import NavBar from 'components/NavBar'
@@ -20,14 +21,24 @@ function FlashCards({ match }) {
   if (maintenance) return <Maintenance />
 
   return (
-    <Switch>
-      <Route exact path={`${match.url}`} render={() => <Home />} />
-      <Route
-        path={`${match.url}/:subject/:domain/:theme/:level`}
-        component={DisplayFlashCards}
-      />
-      <Route render={() => <h1>Erreur</h1>} />
-    </Switch>
+    <div>
+      <Helmet>
+        <title>Ubu Maths - Flash Cards</title>
+        <meta
+          name='description'
+          content='Créer et utiliser des jeux de Flash Cards pour réviser une matière, une notion.'
+        />
+        <meta name='keywords' content='Flash, Cards, math, latex'/>
+      </Helmet>
+      <Switch>
+        <Route exact path={`${match.url}`} render={() => <Home />} />
+        <Route
+          path={`${match.url}/:subject/:domain/:theme/:level`}
+          component={DisplayFlashCards}
+        />
+        <Route render={() => <h1>Erreur</h1>} />
+      </Switch>
+    </div>
   )
 }
 
@@ -49,26 +60,22 @@ function Home() {
           <GridItem xs={12} sm={12} md={3} lg={3}>
             <h2>Flash Cards</h2>
 
-            
-              <SelectSubject
-                subjects={subjects}
-                subject={subject}
-                onChange={setSubject}
-              />
-            
-            
-              <SelectDomain
-                domains={domains}
-                domain={domain}
-                onChange={setDomain}
-              />
-            
-           
-              <SelectTheme themes={themes} theme={theme} onChange={setTheme} />
-           
-            
-              <SelectGrade grades={grades} grade={grade} onChange={setGrade} />
-          
+            <SelectSubject
+              subjects={subjects}
+              subject={subject}
+              onChange={setSubject}
+            />
+
+            <SelectDomain
+              domains={domains}
+              domain={domain}
+              onChange={setDomain}
+            />
+
+            <SelectTheme themes={themes} theme={theme} onChange={setTheme} />
+
+            <SelectGrade grades={grades} grade={grade} onChange={setGrade} />
+
             <ThemesList subject={subject} domain={domain} grade={grade} />
           </GridItem>
         </GridContainer>
