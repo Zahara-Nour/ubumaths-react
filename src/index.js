8baf5981153b4a510c388489c1ec3490f82125f3
David Le Jolly <zahara.alnour@gmail.com>
2020-07-06 10:03:35 +0300

    Cleaning

--- src/index.js
+++ src/index.js
@@ -9,33 +9,6 @@ import { Provider } from 'react-redux'
 import * as serviceWorker from './app/serviceWorker'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
-// import LogRocket from 'logrocket'
-// import setupLogRocketReact from 'logrocket-react'
-import {setDefaultLevel} from 'loglevel'
-
-
-// import * as Sentry from '@sentry/browser'
-// import { version } from '../package.json'
-
-// // if (process.env.NODE_ENV === 'production') {
-// Sentry.init({
-//   dsn:
-//     'https://86906891d533413d89c6b74f6d528727@o397779.ingest.sentry.io/5252881',
-//   release: version,
-// })
-// LogRocket.init('7mzcdm/ubumaths',{
-//   release:version
-// })
-// LogRocket.getSessionURL((sessionURL) => {
-//   Sentry.configureScope((scope) => {
-//     scope.setExtra('sessionURL', sessionURL)
-//   })
-// })
-// }
-
-//setupLogRocketReact(LogRocket)
-
-setDefaultLevel('debug')
 
 const hist = createBrowserHistory()
 
